06831e9218b19417a4fb1737ab56e9ef3cc254e2
Aleksey Shavrak <alekseyshavrak@gmail.com>
2021-12-22 20:15:30 +0300

    fix logo

--- resources/views/vendor/nova/partials/logo.blade.php
+++ resources/views/vendor/nova/partials/logo.blade.php
@@ -1,7 +1,5 @@
 <svg
     class="fill-current"
-    width="{{ $width ?? '126' }}"
-    height="{{ $height ?? '24' }}"
     viewBox="{{ $viewBox ?? '0 0 126 24' }}"
     xmlns="http://www.w3.org/2000/svg"
    fill="none"
